5173be20c68e202e7c8c12d3a01edf4f915c3d1e
juliamariee <48110426+juliamariee@users.noreply.github.com>
2020-08-13 21:32:26 -0400

    Reverted commands to use ! prefix

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ client.on("message", async (msg) => {
   if (msg.guild == '742838820109811712') {
     // REMEMBER!! The guild is the server! if you want to add bot to another server must add server id to this conditional!
 
-    if (msg.content == "-meme") {
+    if (msg.content == "!meme") {
       let ind = Math.floor(Math.random() * memes.meme_list.length)
       let meme_title = memes.meme_list[ind].name;
       await msg.channel.send(`Your meme is ${meme_title}`)
       await msg.channel.send(memes.meme_list[ind].url);
     }
 
-    if (msg.content == "-addmeme") {
+    if (msg.content == "!addmeme") {
       await msg.channel.send({
         embed: {
           title: "Fill out the form to add a meme to the meme bot!",
@@ -27,12 +27,12 @@ client.on("message", async (msg) => {
       });
     }
 
-    if (msg.content == "-help") {
+    if (msg.content == "!help") {
       await msg.channel.send({
         embed: {
           color: 3447003,
           title: "Commands for meme bot:",
-          description: `-meme = get a random meme sent to chat\n-addmeme = instructions for how to add memes to the bot\n-help = view all meme bot commands`
+          description: `!meme = get a random meme sent to chat\n!addmeme = instructions for how to add memes to the bot\n!help = view all meme bot commands`
         }
       });
     }
